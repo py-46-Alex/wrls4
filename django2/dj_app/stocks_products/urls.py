c49bee3587e9ae222cf31b70e5cb222baf1ffa97
alex12mb@mail.ru <alex12mb@mail.ru>
2023-02-27 15:50:27 +0300

    first commit

---
+++ django2/dj_app/stocks_products/urls.py
@@ -0,0 +1,12 @@
+"""stocks_products URL Configuration
+
+Tlude() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('logistic.urls')),
+]
